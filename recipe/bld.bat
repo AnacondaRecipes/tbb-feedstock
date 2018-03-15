6a65f2ca37dd85701949f03d080daad739f44eba
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-15 16:17:17 -0500

    Windows: Fix build, add reason for preferring NMake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,19 @@
 
 @rem See https://github.com/wjakob/tbb
-cp "%RECIPE_DIR%\CMakeLists.txt" .
+copy "%RECIPE_DIR%\CMakeLists.txt" .
 if errorlevel 1 exit 1
-cp "%RECIPE_DIR%\version_string.ver.in" build
+copy "%RECIPE_DIR%\version_string.ver.in" build
 if errorlevel 1 exit 1
 
 mkdir buildw && cd buildw
 
 set CMAKE_CONFIG="Release"
 
-cmake -LAH -G"%CMAKE_GENERATOR%"                             ^
+@rem Not using %CMAKE_GENERATOR% here intentionally, as it causes CMake to use
+@rem C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/x86_amd64/ml64.exe
+@rem instead of C:/Program Files (x86)/Microsoft Visual Studio 9.0/VC/bin/amd64/ml64.exe
+@rem and the former chokes on asm files meant for 64 bit code.
+cmake -LAH -G"NMake Makefiles"                             ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%"                ^
   -DCMAKE_BUILD_TYPE="%CMAKE_CONFIG%"                      ^
   -DTBB_CI_BUILD=ON                                        ^
